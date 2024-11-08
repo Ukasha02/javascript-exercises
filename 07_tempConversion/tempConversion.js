7a634d11e0df5a24d7f6c406b4eddb283caf00df
Ukasha Khalid <ukashakhalid3@gmail.com>
2024-11-09 00:57:32 +0500

    Done till Qs7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp1) {
+  let c_temp = ((temp1 - 32) * (5/9));
+  let rounded_temp =  Math.round(c_temp * 10) / 10;
+  return rounded_temp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp2) {
+  let f_temp = ((temp2 * (9/5))+ 32);
+  let rounded_temp2 =  Math.round(f_temp * 10) / 10;
+  return rounded_temp2
 };
 
 // Do not edit below this line
